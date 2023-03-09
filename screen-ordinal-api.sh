bf44f89660532618e6615a0679fd760638a96443
Azri Fadzil <afwcxx@gmail.com>
2023-03-09 14:03:48 +0800

    bash script update

--- screen-ordinal-api.sh
+++ screen-ordinal-api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 screen -X -S ordinal-api quit
-screen -S ordinal-api -dm SCRIPT_DIR/start-ordinal-api.sh
+screen -S ordinal-api -dm $SCRIPT_DIR/start-ordinal-api.sh
 
--- start-ordinal-api.sh
+++ start-ordinal-api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd SCRIPT_DIR
+cd $SCRIPT_DIR
 node app.js
 
