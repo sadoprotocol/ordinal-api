b738d8d9bcf9e746375d166bb83f7b6b0430a49b
Azri Fadzil <afwcxx@gmail.com>
2023-04-12 11:20:16 +0800

    cron indexer update - also performs copier ord roles

--- ord-index.sh
+++ ord-index.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
+# Note:
+# This index checking is only used for double database location
+# where the one who is indexing will keep the data in a separate location.
+# Once done with indexing, it should transfer to the original location
 
-if ps -fu $UID | grep "ord" | grep "index-sats index"
+NETWORK="regtest" # Leave empty string for Mainnet
+DEFAULT_DATA_DIR="/home/bitcoin/.local/share/ord/"
+ALT_DATA_DIR="/home/bitcoin/ord-data/"
+
+if ps -fu $UID | grep "ord" | grep "data-dir" | grep -q "index-sats index"
 then 
-   echo "Still running..";
+   echo "Still running.."
 else
-   echo "Begin indexing..";
-   ord@afwcxx --index-sats index
+   # Change the -r flag for NETWORK
+   # -r : regtest
+   # -t : regtest
+   # <nothing> : mainnet
+
+   echo "Begin indexing.."
+   ord@afwcxx -r --data-dir=$ALT_ORD_DIR --index-sats index
+
+   echo "Transferring to original location"
+   \cp "$ALT_DATA_DIRindex.redb" "$DEFAULT_DATA_DIRindex.redb.new"
+   mv -f "$DEFAULT_DATA_DIRindex.redb.new" "$DEFAULT_DATA_DIRindex.redb"
+
+   echo "Done.."
 fi
 
