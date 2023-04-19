e05dd1d78ff24f4b42b958645a491061ab06534b
Azri Fadzil <afwcxx@gmail.com>
2023-04-19 10:36:19 +0800

    snapshot creation and cleanup

--- ord-index.sh
+++ ord-index.sh
@@ -5,27 +5,57 @@
 # Once done with indexing, it should transfer to the original location
 
 # TODO
-NETWORK="regtest" # Leave empty string for Mainnet
+NETWORK="regtest" # OR "testnet3", "" for Mainnet
 DEFAULT_DATA_DIR="/home/bitcoin-regtest/.local/share/ord/"
 ALT_DATA_DIR="/home/bitcoin-regtest/ord-data/"
 
 if ps -fu $UID | grep "ord" | grep "data-dir" | grep -q "index-sats index"
 then 
-   echo "Still running.."
+  echo "Still running.."
 else
-   # Change the -r flag for NETWORK
-   # -r : regtest
-   # -t : regtest
-   # <nothing> : mainnet
+  echo "Begin indexing.."
+  # TODO
+  # Change the -r flag for NETWORK
+  # -r : regtest
+  # -t : testnet3
+  # <nothing> : mainnet
+  REORG=$(ord@afwcxx -r --data-dir=$ALT_DATA_DIR --index-sats index | grep -P 'reorg')
 
-   echo "Begin indexing.."
-   # TODO
-   ord@afwcxx -r --data-dir=$ALT_DATA_DIR --index-sats index
+  echo "REORG is $REORG"
 
-   echo "Transferring to original location"
-   \cp $ALT_DATA_DIR$NETWORK"/index.redb" $DEFAULT_DATA_DIR$NETWORK"/index.redb.new"
-   mv -f $DEFAULT_DATA_DIR$NETWORK"/index.redb.new" $DEFAULT_DATA_DIR$NETWORK"/index.redb"
+  if [ -z "$REORG" ]
+  then
+    # TODO
+    HEIGHT=$(bitcoin-cli --regtest getblockcount)
 
-   echo "Done.."
+    echo "HEIGHT is $HEIGHT"
+
+    MOD=$(expr $HEIGHT % 5)
+
+    echo "MOD is $MOD"
+
+    if [ $MOD -eq 0 ];
+    then
+      STALE=$(expr $HEIGHT - 50)
+      echo "STALE is $STALE"
+      echo "Creating snapshot"
+      \cp $ALT_DATA_DIR$NETWORK"/index.redb" $ALT_DATA_DIR$NETWORK"/index.redb.$HEIGHT"
+
+      if test -f "$ALT_DATA_DIR$NETWORK/index.redb.$STALE"; then
+        echo "Removing stale"
+        rm $ALT_DATA_DIR$NETWORK"/index.redb.$STALE"
+        echo "Removed stale"
+      fi
+
+    fi
+
+    echo "Transferring to original location"
+    \cp $ALT_DATA_DIR$NETWORK"/index.redb" $DEFAULT_DATA_DIR$NETWORK"/index.redb.new"
+    mv -f $DEFAULT_DATA_DIR$NETWORK"/index.redb.new" $DEFAULT_DATA_DIR$NETWORK"/index.redb"
+
+    echo "Done.."
+  else
+    echo "Has reorg.."
+  fi
 fi
 
